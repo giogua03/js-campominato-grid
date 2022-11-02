732d3f8e5a3aa3ac2272c17d628d46fbc7f726cc
giogua03 <giorgiogua03@gmail.com>
2022-11-02 17:05:50 +0100

    bonus

--- scrypt.js
+++ scrypt.js
@@ -1,17 +1,54 @@
-const eleGrid = document.querySelector('.big-q');
+const eleBig = document.querySelector('.big-q');
 const eleButton = document.querySelector('button')
-
+let eleOption = document.querySelector('value')
+if (eleOption == 1){
 eleButton.addEventListener('click', function(){
 
 let i
 
 for (let i = 1; i < 101; i++) {
-    const eleCell = document.createElement( 'div');
-	eleCell.classList.add('small-q');
-	eleGrid.append(eleCell);
+    const eleSmall = document.createElement( 'div');
+	eleSmall.classList.add('small-q');
+	eleBig.append(eleSmall);
+	eleSmall.append([i]);
 
-	eleCell.addEventListener('click', function () {
+	eleSmall.addEventListener('click', function () {
 		this.classList.toggle('active')
 	});
 }
 });
+} else  if (eleOption == 2){ 
+	eleButton.addEventListener('click', function(){
+
+		let i
+		
+		for (let i = 1; i < 82; i++) {
+			const eleSmall = document.createElement( 'div');
+			eleSmall.classList.add('small-q');
+			eleBig.append(eleSmall);
+			eleSmall.append([i]);
+		
+			eleSmall.addEventListener('click', function () {
+				this.classList.toggle('active')
+			});
+		}
+		});
+
+} else {
+	eleButton.addEventListener('click', function(){
+
+		let i
+		
+		for (let i = 1; i < 50; i++) {
+			const eleSmall = document.createElement( 'div');
+			eleSmall.classList.add('small-q');
+			eleBig.append(eleSmall);
+			eleSmall.append([i]);
+		
+			eleSmall.addEventListener('click', function () {
+				this.classList.toggle('active')
+			});
+		}
+		});
+
+}
