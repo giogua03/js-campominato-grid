d5cd6061a76fb08333252dae3b88c89794081d2f
giogua03 <giorgiogua03@gmail.com>
2022-11-02 17:55:59 +0100

    bonus-finito

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,10 @@
 const eleBig = document.querySelector('.big-q');
 const eleButton = document.querySelector('button')
-let eleOption = document.querySelector('value')
-if (eleOption == 1){
+
 eleButton.addEventListener('click', function(){
+let eleOption = document.querySelector('#list').value
+if (eleOption == "1"){
+
 
 let i
 
@@ -15,40 +17,46 @@ for (let i = 1; i < 101; i++) {
 	eleSmall.addEventListener('click', function () {
 		this.classList.toggle('active')
 	});
+	
+		
+	
 }
-});
-} else  if (eleOption == 2){ 
-	eleButton.addEventListener('click', function(){
+
+} else  if (eleOption == "2"){ 
+	
 
 		let i
 		
 		for (let i = 1; i < 82; i++) {
 			const eleSmall = document.createElement( 'div');
-			eleSmall.classList.add('small-q');
+			eleSmall.classList.add('small-e');
 			eleBig.append(eleSmall);
 			eleSmall.append([i]);
 		
 			eleSmall.addEventListener('click', function () {
 				this.classList.toggle('active')
 			});
+			
 		}
-		});
+		
 
 } else {
-	eleButton.addEventListener('click', function(){
+	
 
 		let i
 		
 		for (let i = 1; i < 50; i++) {
 			const eleSmall = document.createElement( 'div');
-			eleSmall.classList.add('small-q');
+			eleSmall.classList.add('small-d');
 			eleBig.append(eleSmall);
 			eleSmall.append([i]);
 		
 			eleSmall.addEventListener('click', function () {
 				this.classList.toggle('active')
 			});
+			
 		}
-		});
+		
 
 }
+});
